fe0377ddda26cfdcc1e2d13e16704af9bc78603e
bkoch <bkoch4142@users.noreply.github.com>
2021-02-21 16:46:29 +0100

    update readme and test_annotation script

---
+++ README.md
@@ -0,0 +1,19 @@
+# YOLOv5 with Synthetic data
+Training/Application of the YOLOv5 object detection algorithm on a synthetic dataset generated in Unity and its Perception module
+
+## Perception module 
+Unity Perception module tutorial is available at:
+
+https://github.com/Unity-Technologies/com.unity.perception/blob/master/com.unity.perception/Documentation~/Tutorial/Phase1.md
+
+## YOLOv5
+YOLOv5 implementation used in this repository is available at:
+
+https://github.com/ultralytics/yolov5
+
+## Results
+
+<p align="center">
+    <img width="800" height="700" src="./other/mosaic.jpg">
+</p>
+
--- scripts/test_annotation.py
+++ scripts/test_annotation.py
@@ -3,8 +3,8 @@ import cv2
 import matplotlib.pyplot as plt
 
 image='rgb_2'
-anno_file='data/detections_yolo/txt/rgb_2.txt'
-image_file='data/synth_data/rgb/rgb_2.png'
+anno_file='data/prepared/detections_yolo/txt/rgb_2.txt'
+image_file='data/raw/synth_data/rgb/rgb_2.png'
 
 IMG_WIDTH=933
 IMG_HEIGHT=700
@@ -27,7 +27,7 @@ for bb in bbs:
     y=int((float(bb[1]))*IMG_HEIGHT)
     cv2.circle(image, (x,y), radius=1, color=(255, 0, 0), thickness=10)
 
-    #turn from yolo format x_center, y_center, w , h(0-1) 
+    # turn from yolo format x_center, y_center, w , h(0-1) 
     # to opecv format x_left,y_up, w ,h(0-int pixel_count)
     x=int((float(bb[0])-float(bb[2])/2)*IMG_WIDTH) #uppper left
     y=int((float(bb[1])-float(bb[3])/2)*IMG_HEIGHT) #upper left
